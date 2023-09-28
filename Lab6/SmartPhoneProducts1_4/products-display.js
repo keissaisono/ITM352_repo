9b4e75c2445b7f315df6667add49d225d0e9c8d0
keissaisono <keissa@hawaii.edu>
2023-09-27 23:14:25 -1000

    update

--- Lab6/SmartPhoneProducts1_4/products-display.js
+++ Lab6/SmartPhoneProducts1_4/products-display.js
@@ -5,7 +5,7 @@ top_title.innerHTML=(store_name + "'s Used Smart Phone Store");
 //send store name info to the footer title
 bottom_title.innerHTML=("Your one stop shop for used phones - "+store_name+"'s");
 
-//intialize hits and spins and send to the DOM
+//initialize hits and spins and send to the DOM
 let hits= 0;
 let spins=0;
 let wins;
@@ -21,33 +21,118 @@ function changeClassName(element) {
         spins=spins+1;
     }
 
-    element.className = 'item rotate';
-    spins=spins+1; 
-    spins_span.innerHTML = spins;
-    hit_spin_span.innerHTML=(hits/spins).toFixed(2);
+    //element.className = 'item rotate';
+    //spins=spins+1; 
+    //spins_span.innerHTML = spins;
+    //hit_spin_span.innerHTML=(hits/spins).toFixed(2);
+
     if(spins<2*hits&&hits<spins){
-        wins=true;
+        //wins=true;
         over_half=true;
     } else {
-        wins=false;
+        //wins=false;
     }
     //win_span.innerHTML=wins;
     win_span.innerHTML=over_half;
+    spins_span.innerHTML = spins;
+    hit_spin_span.innerHTML=Number(hits/spins).toFixed(2)
+    // -- Winning progress depends on hits/spins
+    let hits_spins_ratio = hits/spins;
+    let progress;
+        /*if ( hits_spins_ratio > 0 ) {
+            progress = 'On your way!';
+            if ( hits_spins_ratio >= 0.25 ) {
+                progress = 'Almost there!';
+                if ( hits_spins_ratio >= 0.5 ) {
+                    if( hits < spins) { 
+                        progress = 'You win!';
+                 }
+            }
+        }   
+    }
+    else {
+        progress = 'Get going!' ;
+    }*/
+    if( hits_spins_ratio >= 0.5 && hits<spins ) {
+        progress='You win!';
+    } else if(hits_spins_ration >= 0.25 ){
+        progress='Almost there!';
+    } else if(hits_spins_ratio>0){
+        progress='On your way!';
+    }
+    else {
+        progress='Get going!';
+    }
+
+    win_span.innerHTML=progress;
+    
+//win_span.innerHTML=wins;
+    win_span.innerHTML=over_half;
 }
+
+
 
 //this should be two functions or more but... it changes the item name by removing the rotate for the images and updates the its count and calculates hits/spins ratio, sending both to the DOM
 function resetClassName(element) {
-    element.className = 'item';
-    hits=hits+=2;
-    hits_span.innerHTML = hits;
-    hit_spin_span.innerHTML=(hits_span.innerHTML/
-    spins_span.innerHTML).toFixed(2);
+    if(element.className = 'item'){
+        element.className = 'item';
+        hits=hits+=2;
+    } else {
+        changeClassName(element);
+    }
+    
     if(spins<2*hits&&hits<spins){
-        wins=true;
+        //wins=true;
         over_half=true;
     } else {
-        wins=false;
+        //wins=false;
     }
+    //win_span.innerHTML=wins;
+    win_span.innerHTML=over_half;
+    hits_span.innerHTML = hits;
+    hit_spin_span.innerHTML=Number(hits/spins).toFixed(2)
+    // -- Winning progress depends on hits/spins
+    let hits_spins_ratio = hits/spins;
+    let progress;
+        /*if ( hits_spins_ratio > 0 ) {
+            progress = 'On your way!';
+            if ( hits_spins_ratio >= 0.25 ) {
+                progress = 'Almost there!';
+                if ( hits_spins_ratio >= 0.5 ) {
+                    if( hits < spins) { 
+                        progress = 'You win!';
+                 }
+            }
+        }   
+    }
+    else {
+        progress = 'Get going!' ;
+    }*/
+
+    if( hits_spins_ratio >= 0.5 && hits<spins ) {
+            progress='You win!';
+    } else if(hits_spins_ration >= 0.25 ){
+        progress='Almost there!';
+    } else if(hits_spins_ratio>0){
+        progress='On your way!';
+    }
+    else {
+        progress='Get going!';
+    }
+    win_span.innerHTML=progress;
+    
+
+
+//win_span.innerHTML=wins;
+    win_span.innerHTML=over_half;
+    spins_span.innerHTML=.toFixed(2);
+    if(spins<2*hits&&hits<spins){
+        //wins=true;
+        over_half=true;
+    } else {
+        //wins=false;
+    }
+
     //win_span.innerHTML=wins;
     win_span.innerHTML=over_half;
 }
