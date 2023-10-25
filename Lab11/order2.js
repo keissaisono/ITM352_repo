682f577446b3ae922ce55c78d410bd2b9b6bef3c
keissaisono <keissa@hawaii.edu>
2023-10-24 22:59:57 -1000

    update

--- Lab11/order2.js
+++ Lab11/order2.js
@@ -1,7 +1,7 @@
 function updateQuantityMessage(textbox) {
-    let quantityMessage=document.getElementById('qty_textbox_message');
+    let quantityMessage = document.getElementById('qty_textbox_message');
 
-    //validate the quantity entered
+    //validate quantity entered
     let validationMessage = validateQuantity(Number(textbox.value));
 
     //if there are validation errors, display error message
@@ -10,10 +10,8 @@ function updateQuantityMessage(textbox) {
     } else {
         quantityMessage.innerHTML = textbox.value;
     }
+    }
 
-
-    
-}
 
 
 
@@ -22,38 +20,34 @@ function validateQuantity(quantity) {
 
     switch (true) {
         case isNaN(quantity):
-            errorMessage = "Not a number. Please enter a non-negative quantity to order.";
-            break;
-        case quantity < 0 && !Number.isInteger(quantity):
-            errorMessage = "Negative inventory and not an Integer. Please enter a non-negative quantity to order.";
-            break;
+        errorMessage = "Not a number. Please enter a non-negative quantity to order.";
+        break;
+        case quantity <= 0 && !Number.isInteger(quantity):
+        errorMessage = "Negative inventory and not an Integer. Please enter a non-negative quantity to order.";
+        break;
         case quantity <= 0:
-            errorMessage = "Negative inventory. Please enter a non-negative quantity to order.";
-            break;
+        errorMessage = "Negative inventory. Please enter a non-negative quantity to order.";
+        break;
         case !Number.isInteger(quantity):
-            errorMessage = "Not an Integer. Please enter a non-negative quantity to order.";
-            break;
+        errorMessage = "Not an Integer. Please enter a non-negative quantity to order.";
+        break;
         default:
-            errorMessage = "";
+            errorMessage = ""; //No errors
             break;
     }
-
     return errorMessage;
-}
 
+}
 function displayPurchase() {
     let quantity = Number(document.getElementById('qty_textbox').value);
 
     let validationMessage = validateQuantity(quantity);
-
-    if (validationMessage == "") {
-        let message = "Thank you for ordering ${quantity} things!";
+    if (validationMessage =="") {
+        let message = `Thank you for ordering ${quantity} things!`;
         document.body.innerHTML = message;
     } else {
         alert(validationMessage + " Please enter a positive integer for quantity.");
-        
-        document.getElementById('qty_textbox').value = "";
+        document.getElementById('qty_textbox').value= "";
     }
 
 }
-
