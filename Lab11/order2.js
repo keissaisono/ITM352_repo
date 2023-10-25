d9f952716747712e1fb6e908fb79ad59c889b22d
keissaisono <keissa@hawaii.edu>
2023-10-24 22:36:21 -1000

    update

--- Lab11/order2.js
+++ Lab11/order2.js
@@ -42,7 +42,7 @@ function validateQuantity(quantity) {
 }
 
 function displayPurchase() {
-    let quantity = Number(document.getElementById('qty_textbox_message').value);
+    let quantity = Number(document.getElementById('qty_textbox').value);
 
     let validationMessage = validateQuantity(quantity);
 
