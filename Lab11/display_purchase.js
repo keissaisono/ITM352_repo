64d8989e60415b09ff4bfdafbfdaa5ae41226ab2
keissaisono <keissa@hawaii.edu>
2023-10-25 19:26:20 -1000

    update

--- Lab11/display_purchase.js
+++ Lab11/display_purchase.js
@@ -1,5 +1,5 @@
-let params = (new URL(document.location)).searchParams;
+let params= (new URL(document.location)).searchParams;
 let quantity = params.get("qty_textbox");
 
-let message = 'Thank you for ordering ${quantity} things!';
+let message = `Thank you for ordering ${quantity} things!`;
 document.body.innerHTML = message;
