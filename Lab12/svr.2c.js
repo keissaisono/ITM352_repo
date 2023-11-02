94d62d0385358782d495ea64c2592c1c39c5fec5
keissaisono <keissa@hawaii.edu>
2023-11-01 23:08:07 -1000

    update

--- Lab12/svr.2c.js
+++ Lab12/svr.2c.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+
 // part 2a
 app.get('/test', function (req, res) {
     res.send('app get for test was executed');
@@ -11,4 +14,7 @@ app.all('*', function (request, response, next) {
     //response.send(request.method + ' to path ' + request.path);
     console.log(request.method + ' to path ' + request.path);
 });
+
+
+
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
