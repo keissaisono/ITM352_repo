d4bf678d30797770932c3445aa7cacf58c1260d5
keissaisono <keissa@hawaii.edu>
2023-11-02 19:55:02 -1000

    update

--- Lab12/svr.4.js
+++ Lab12/svr.4.js
@@ -10,17 +10,32 @@ app.get('/test', function (req, res) {
     console.log('app.get for test was executed');
 });
 
+let products = require(__dirname + '/products.json');
+products.forEach( (prod,i) => {prod.total_sold = 0});
+
+app.get("/products.js", function (request, response, next) {
+   response.type('.js');
+   let products_str = `let products = ${JSON.stringify(products)};`;
+   response.send(products_str);
+});
+
 app.use(express.urlencoded({ extended: true }));
 
-
 app.post("/process_form", function (request, response) {
+    let brand = products[0]['brand'];
+    let brand_price = products[0]['price'];
+    
     //response.send(request.body); 
     let q = Number(request.body['qty_textbox']);
     console.log("the input value is ..." +q);
+
+    //increments the number of items sold for the first item in products
+    products[0].total_sold +=q;
+
     let validationMessage = validateQuantity(q);
 
     if (validationMessage === "") {
-        response.send(`Thank you for purchasing ${q} things!`);
+        response.send(`<h2>Thank you for purchasing ${q} ${brand}. Your total is \$${q * brand_price}!</h2>`);
     } else {
         response.send(validationMessage+'<br>'+`Error: ${q} is not a quantity. Hit the back button to fix.`);
     }
@@ -32,6 +47,7 @@ app.all('*', function (request, response, next) {
     //response.send(request.method + ' to path ' + request.path);
     console.log(request.method + ' to path ' + request.path);
 });
+
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
 
