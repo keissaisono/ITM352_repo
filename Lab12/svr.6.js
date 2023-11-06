18d69b23c8eacf5da2ad8ce6f1d61730747bf1f2
keissaisono <keissa@hawaii.edu>
2023-11-05 20:45:39 -1000

    update

--- Lab12/svr.6.js
+++ Lab12/svr.6.js
@@ -1,62 +1,101 @@
-//svr.6.js
-
 let express = require('express');
 let app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-// Load products data
-const products = require(__dirname + '/products.json');
-products.forEach((prod) => {
-  prod.total_sold = 0;
+app.get('/test', function(req, res) {
+    res.send('app.get for test was executed');
+    console.log('app.get for test was executed');
 });
 
-// Use body-parser to parse form data
+let products = require(__dirname + '/products.json');
+products.forEach( (prod,i) => {prod.total_sold = 0});
+
+
+app.get("/products.js", function (request, response, next) {
+    response.type('.js');
+    let products_str = `let products = ${JSON.stringify(products)};`;
+    response.send(products_str);
+ });
+
 app.use(express.urlencoded({ extended: true }));
 
-let receipt = '';
-    let qtys = request.body[`quantity_textbox`];
-    for (i in qtys) {
-        let q = qtys[i];
+//Posting the form data
+/*app.post("/process_form", function (request, response) {
+    let receipt = '';
+    //let qtys = Number(request.body[`quantity_textbox${i}`]);
+    //console.log(qtys);
+    //for (let i in qtys) {
+        for (let i in products) {
+        let qtys = request.body[`quantity_textbox${i}`];
+        let q = Number(qtys[i]);
+        console.log("the quantity value is " +q);
+        let validationMessage = validateQuantity(q);
         let brand = products[i]['brand'];
         let brand_price = products[i]['price'];
         if (validateQuantity(q)==="") {
             products[i]['total_sold'] += Number(q);
-            receipt += `<h3>Thank you for purchasing: ${q} ${brand}. Your total is \$${q * brand_price}!</h3>`; // render template string
+            receipt += `<h3>Thank you for purchasing: ${q} ${brand}. Your total is \$${q * brand_price}!</h3>`;   // render template string
         } else {
-            receipt += `<h3><font color="red">${q} is not a valid quantity for ${brand}!</font></h3>`;
+            receipt += `<h3><font color="red">${q} is not a valid quantity for ${brand}!<br>${validationMessage}</font></h3>`;
         }
     }
     response.send(receipt);
     response.end();
-
-app.all('*', function (request, response, next) {
-  console.log(request.method + ' to path ' + request.path);
-  next(); // Make sure to call next() to continue processing the request
+*/
+app.post("/process_form", function (request, response) {
+    let receipt = '';
+    for (let i in products) {
+        let qtys = Number(request.body[`quantity_textbox${i}`]); // Convert to a number here
+        let q = qtys; // No need for q = Number(qtys[i])
+        console.log("the quantity value is " + q);
+        let validationMessage = validateQuantity(q);
+        let brand = products[i]['brand'];
+        let brand_price = products[i]['price'];
+        if (validateQuantity(q) === "") {
+            products[i]['total_sold'] += q; // Use 'q' here
+            receipt += `<h3>Thank you for purchasing: ${q} ${brand}. Your total is \$${q * brand_price}!</h3>`;
+        } else {
+            receipt += `<h3><font color="red">${q} is not a valid quantity for ${brand}!<br>${validationMessage}</font></h3>`;
+        }
+    }
+    response.send(receipt);
+    response.end();
 });
 
-app.listen(8080, () => console.log(`listening on port 8080`));
 
-function validateQuantity(quantity) {
-  let errorMessage = '';
+app.all('*', function (request, response, next) {
+    //response.send(request.method + ' to path ' + request.path);
+    console.log(request.method + ' to path ' + request.path);
+    next();
+});
 
-  switch (true) {
-    case isNaN(quantity):
-      errorMessage = 'Not a number. Please enter a non-negative quantity to order.';
-      break;
-    case quantity < 0 && !Number.isInteger(quantity):
-      errorMessage = 'Negative inventory and not an integer. Please enter a non-negative quantity to order.';
-      break;
-    case quantity < 0:
-      errorMessage = 'Negative inventory. Please enter a non-negative quantity to order.';
-      break;
-    case !Number.isInteger(quantity):
-      errorMessage = 'Not an integer, Please enter a non-negative quantity to order.';
-      break;
-    default:
-      errorMessage = '';
-      break;
-  }
 
-  return errorMessage;
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
+
+
+
+//Validate Quantity
+function validateQuantity (quantity) {
+    let errorMessage = "";
+
+    switch (true) {
+        case isNaN(quantity):
+        errorMessage = "Not a number. Please enter a non-negative quantity to order.";
+        break;
+        case quantity <= 0 && !Number.isInteger(quantity):
+        errorMessage = "Negative inventory and not an Integer. Please enter a non-negative quantity to order.";
+        break;
+        case quantity <= 0:
+        errorMessage = "Negative inventory. Please enter a non-negative quantity to order.";
+        break;
+        case !Number.isInteger(quantity):
+        errorMessage = "Not an Integer. Please enter a non-negative quantity to order.";
+        break;
+        default:
+            errorMessage = ""; //No errors
+            break;
+    }
+
+    return errorMessage;
 }
