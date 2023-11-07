4c3b2aa8e3337fa2e63b49ae5070ff8f11306bf8
keissaisono <keissa@hawaii.edu>
2023-11-06 17:13:24 -1000

    ASSIGNMENT 1 UPDATE

--- Assignment1/server.js
+++ Assignment1/server.js
@@ -1,25 +1,49 @@
-// Layout taken from Assignment 1 Workshop Module//
-//function (isNonNegInt) taken from example 1 assignment
-// Determines valid quantity (If "q" is a negative integer)
-function isNonNegInt(q, return_errors = false) {
-    errors = []; // assume no errors at first
-    if (q == '') q = 0; // handle blank inputs as if they are 0
-    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
-    if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
-    if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-    return return_errors ? errors : (errors.length == 0);
-};
+// Created by Keissa Isono
+// Server.js used to validate data inputted in the webstore, and path to either an error message on the same page or redirect to the invoice page
 
-
-//Load in query string, product info, and express package
-const qs=require('node:querystring');
+// Declare/get query string as qs to be used later, load product data and express
+const qs = require('node:querystring');
 var products = require(__dirname + '/products.json');
+console.log(products);
+const { query } = require('express');
 var express = require('express');
 var app = express();
 
+// Non Negative Integer function, used later to determine validity, (If q is "")
+function isNonNegInt(q, returnErrors=false) {
+   errors = [];
+   if(Number(q) != q) errors.push('Not a number!');
+   if(q < 0) errors.push('Negative value!');
+   if(parseInt(q) != q) errors.push('Not an integer!');
+   return (returnErrors ? errors : (errors.length == 0));
+   };
+
+// Created function used to check input in the textbox and add an error message below in real time.
+// Used assistance from ChatGPT to tweak the below function from Lab 12 (lines 23 - 41) to dynamically change my error message, as well as match IR3's criteria (red border color, error msg, & replacing input value to the qty_available)
+function checkQuantityTextbox(qtyTextbox) {
+   const qtyAva = parseInt(qtyTextbox.dataset.qtyAva); // Get available quantity from dataset
+   const qty = parseInt(qtyTextbox.value); // Get entered quantity
+   const errorSpan = document.getElementById(qtyTextbox.id + "_errors");
+   // Check if entered quantity exceeds available quantity
+   if (qty > qtyAva) { 
+    // Changes the error message to the following
+     errorSpan.innerHTML = `We don't have ${qty} available`;
+     // Sets textbox value to available quantity
+     qtyTextbox.value = qtyAva; 
+     // Changes textbox border color to red when value is > qtyAva
+     qtyTextbox.style.borderColor = "red"; 
+   } 
+   // Clears error message and changes border color back to default
+   else { 
+     errorSpan.innerHTML = "";
+     qtyTextbox.style.borderColor = "";
+   }
+};
+
+// Middleware from Lab12
 app.use(express.urlencoded({ extended: true }));
 
-// monitor all requests  
+// Monitor all requests
 app.all('*', function (request, response, next) {
    console.log(request.method + ' to ' + request.path);
    next();
@@ -34,49 +58,53 @@ app.get("/products.js", function(request, response, next)
         });
 
 // process purchase request (validate quantities, check quantity available)
-app.post("/purchase", function(request, response, next){
-    console.log(request.body);
-    var q;
-    var has_quantities = false;
-    var errors = {};
-    //advice for later: serialize error object; find better way to pass errors back and forth for Assignment2
-    //Help from Professor Port
-    for (let i in products) {
-        q = request.body['quantity' + i];
-        if (typeof q != 'undefined') {
-            console.log(q);
-            // Check whether there are even quantities that have been inputted
-            if(q>0) {
-                has_quantities = true;
-            }
-            // Validates data with isNonNegInt function
-            if(isNonNegInt(q,false) == false) {
-                errors['quantity_error'+i] = isNonNegInt(q,true);
-            }
-            if (q > products[i].amt_ava) {  //Check to see if there is enough stock left
-                errors['stock_outage' + i ] = `We currently don't have ${(q)} ${products[i].name}s. Please check back later!`
-            }
-        }
-    }
-    // This code is to print out an error stating that the user needs to select quantites instead of leaving it blank
-    if(has_quantities == false) {
-        errors['no_selections_error'] = "Please select some items to purchase!";
-    }
-    // This code is for when there are no errors and will move the user on towards the invoice.html file I have instead of directing them back to products display (like when we do have an error)
-    if (Object.keys(errors).length == 0) {
-        //If quantities are valid, remove quantities from the quantity available.
-        for(let i in products){
-            products[i].amt_ava -= Number(request.body['quantity' + i]);
-        }
-        response.redirect("./invoice.html?" + qs.stringify(request.body));
-    } else {
-        response.redirect("./products_home.html?" +  qs.stringify(request.body) + '&' + qs.stringify(errors));
-    }
+app.post('/purchase', function (request, response, next) {
+//Receive data from textboxes and log
+console.log(request.body);
+
+// Below code (lines 67 - 103) based on Branson Suzuki's (F22) server.js 
+// Declaring q as a empty variable, setting the has_quantity default to false (eg. quantities haven't been entered yet), and an empty errors object.
+   var q
+   var has_quantity = false;
+   var errors = {};
+
+for (let i in products) {
+   q = request.body['quantity' + i];
+   if (typeof q != 'undefined') {
+   console.log(q);
+   // Check that there were quantities inputted
+   if(q > 0) {
+      has_quantity = true;
+   }
+   // Using isNonNegInt to validate values
+   if(isNonNegInt(q) == false) {
+       errors['quantity_error'+i] = isNonNegInt(q,true);
+   }
+// Checking stock validity, (created pre - IR3 code, as the IR3 code blocks the user from sending in a quantity larger than the qty_available, but still functions properly)
+   if (q > products[i].qty_ava) {  
+       errors['stock_outage' + i ] = `We currently don't have ${(q)} ${products[i].name}s. Please check back later!`
+   }
+}
+}
+// Prints an error telling the user to select an item to purchase; when the user hasn't input any values.
+if(has_quantity == false) {
+errors['no_selections_error'] = "Please select some items to purchase!";
+}
+// If all selected quantities are valid, and at least one selection is made without errors, redirect to the invoice.html file, and in all other cases it will stay on the store page.
+if (Object.keys(errors).length == 0) {
+// If the selected quantities are valid, it will take the quantity purchased out of the quantity available.
+for(let i in products){
+   products[i].qty_ava -= Number(request.body['quantity' + i]);
+}
+response.redirect("./invoice.html?" + qs.stringify(request.body));
+} else {
+response.redirect("./products_display.html?" +  qs.stringify(request.body) + '&' + qs.stringify(errors));
+}
 });
 
 
 // route all other GET requests to files in public 
-app.use(express.static('./public'));
+app.use(express.static(__dirname + '/public'));
 
 // start server
 app.listen(8080, () => console.log(`listening on port 8080`));
